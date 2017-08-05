83703da5c957e79adb3cd14213fceefc0d7e0cfa
海老原宏樹 <ebiharahiroki@ebiharahiroki-no-MacBook-Pro.local>
2017-08-05 13:20:22 +0900

    first commit

---
+++ src/main/java/com/example/herokudatabasetest/HerokuDatabaseTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.herokudatabasetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDatabaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDatabaseTestApplication.class, args);
+	}
+}
